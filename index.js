a996c370ffca1f235cf52ac9d7ee81239ae8709b
majazocom <maja.gedda@zocom.se>
2023-01-31 10:28:40 +0100

    fetch multiple imgs

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-const ACCESS_KEY = '';
+const ACCESS_KEY = 'kfOPnNCgLZJepDunOg9Vt6RlMf1t8BybcxZbjtgkRXY';
+const ACCESS_KEY2 = 'YisUcdG_-r1zyERJmgSH94sqSPS_o6-LtsdLlgTeZf8';
+const ACCESS_KEY3 = 'V2_qc3wlqzic140eJ_PsKoIoOm6TmRueFrGzdcKqbXU';
 const start_URL = 'https://api.unsplash.com';
 
 async function getRandomImages() {
     // vår fetch mot API'et
+    // fetch är async
     // '/photos/random' är vår endpoint
-    let data = await fetch(start_URL + '/photos/random/?client_id=' + ACCESS_KEY);
+    // '&count=10' betyder att vi skickar in en parameter där vi vill ha 10 st bilder
+    let data = await fetch(start_URL + '/photos/random/?client_id=' + ACCESS_KEY + '&count=10');
     data = await data.json();
+    console.log(data);
 
-    let imageObject = {
-        alt: data.alt_description,
-        img_url: data.urls.small
-    }
-
-    let imgEl = document.querySelector('#random-image');
-    imgEl.setAttribute('alt', imageObject.alt);
-    imgEl.setAttribute('src', imageObject.img_url);
+    // fetchen returnerar en lista med objekt där vi har information som vi vill ha
+    
 };
 
 getRandomImages();
