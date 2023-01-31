7e0ac82070836a98e66027eee28dcb21a341bdc5
majazocom <maja.gedda@zocom.se>
2023-01-31 11:57:19 +0100

    hide keys etc

--- index.js
+++ index.js
@@ -1,33 +1,47 @@
-const ACCESS_KEY = 'kfOPnNCgLZJepDunOg9Vt6RlMf1t8BybcxZbjtgkRXY';
-const ACCESS_KEY2 = 'YisUcdG_-r1zyERJmgSH94sqSPS_o6-LtsdLlgTeZf8';
-const ACCESS_KEY3 = 'V2_qc3wlqzic140eJ_PsKoIoOm6TmRueFrGzdcKqbXU';
+const ACCESS_KEY = '';
+const ACCESS_KEY2 = '';
+const ACCESS_KEY3 = '';
 const start_URL = 'https://api.unsplash.com';
 const searchButton = document.querySelector('#search-button');
 const searchInput = document.querySelector('#search-input');
 
 // eventlyssnare på vår sökknapp
-searchButton.addEventListener('click', () => {
-    console.log(searchInput.value);
-    //getImages();
+searchButton.addEventListener('click', (event) => {
+    // för att inte genomföra default actions som sker vid knapptryck på submitknapp i form
+    event.preventDefault();
+    getImages(searchInput.value);
 });
 
-async function getImages() {
+function getImages(searchTerm) {
     // vår fetch mot API'et
     // fetch är async
     // '/photos/random' är vår endpoint
+    // för att gå mot search-endpointen som api'et har; '/search/photos/'
     // '&count=10' betyder att vi skickar in en parameter där vi vill ha 10 st bilder
-    let data = await fetch(start_URL + '/photos/random/?client_id=' + ACCESS_KEY2 + '&count=10');
-    data = await data.json();
+    fetch(start_URL
+        + '/search/photos/?client_id=' + ACCESS_KEY2
+        + '&per_page=3&query=' + searchTerm).then(data => data.json()).then((data) => {
+            // url: https://api.unsplash.com/search/photos/?client_id=<ACCESS_KEY>&count=10&query=cats
 
-    // fetchen returnerar en lista med objekt där vi har information som vi vill ha
-    data.forEach(function(image) {
-        // för varje element i listan vill vi trycka ut den i vårt UI
-        console.log(image);
-        // måste skapa vårt nya element i js först
-        let img = document.createElement('img');
-        img.setAttribute('src', image.urls.small);
-        img.setAttribute('alt', image.alt_description);
-        // för att den skall synas i ui behöver vi appenda det till ett redan existerande element där
-        document.querySelector('.images-container').appendChild(img);
-    });
+            // töm elementet som innehåller bilder, ifall det finns tidigare värden och man söker igen
+            document.querySelector('.images-container').innerHTML = "";
+
+            // fetchen returnerar en lista med objekt där vi har information som vi vill ha
+            data.results.forEach(function (image) {
+                // för varje element i listan vill vi trycka ut den i vårt UI
+                // måste skapa vårt nya element i js först
+                let img = document.createElement('img');
+                img.setAttribute('src', image.urls.small);
+                img.setAttribute('alt', image.alt_description);
+                // för att den skall synas i ui behöver vi appenda det till ett redan existerande element där
+                document.querySelector('.images-container').appendChild(img);
+            });
+        })
 };
+
+// create promise
+let promise = new Promise(function(resolve, reject) {
+    resolve('done');
+    reject(new Error('du suger')); // ignorerat
+    setTimeout(() => resolve('OK'), 1000); // ignorerad
+});
